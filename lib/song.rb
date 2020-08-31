f59cc46daa77fd372aea6b62741da5bd0c79a790
Marques Avery <averymarques4@gmail.com>
2020-08-31 02:16:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,12 +9,12 @@ class Song
   @@all = []
 
   def initialize
-    @@songs << self
+    @@all << self
   end
 
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def self.find_by_name(name)
+  #   @@songs.detect{|a| a.name == name}
+  # end
 
   def self.all
     @@all
